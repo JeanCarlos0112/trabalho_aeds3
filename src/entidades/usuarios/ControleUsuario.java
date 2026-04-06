280a29086aafe42c31e1485c6ab223ebc4a21922
Luiz Fernando Santos Langa <luizfernandosantoslanga2006@gmail.com>
2026-04-05 21:14:23 -0300

    User control and view v1.0

--- src/entidades/usuarios/ControleUsuario.java
+++ src/entidades/usuarios/ControleUsuario.java
@@ -1,5 +1,64 @@
 package entidades.usuarios;
 
+import java.util.ArrayList;
+import entidades.cursos.*;
+
 public class ControleUsuario {
+    private ArquivoUsuario arqUsuario;
+    private ArquivoCurso arqCurso;
+
+    public ControleUsuario() throws Exception {
+        this.arqUsuario = new ArquivoUsuario();
+        this.arqCurso = new ArquivoCurso();
+    }
+
+    public boolean cadastrarUsuario(String nome, String email, int hashSenha, String perguntaSecreta, int hashRespostaSecreta) throws Exception {
+        // Verifica se o email escolhido já está em uso por outra pessoa
+        if (arqUsuario.readEmail(email) != null) {
+                return false; // Email já cadastrado
+        }
+
+        Usuario novoUsuario = new Usuario(nome, email, hashSenha, perguntaSecreta, hashRespostaSecreta);
+        arqUsuario.create(novoUsuario);
+
+        return true;
+    }
+
+    public Usuario logarUsuario(String email, int hashSenhaTentativa) throws Exception {
+        Usuario login = arqUsuario.readEmail(email);
+        if(login != null && login.getHashSenha() == hashSenhaTentativa) {
+            return login; // Login bem-sucedido
+        }
+
+        return null; // Credenciais inválidas
+    }
+
+    public boolean atualizarUsuario(Usuario usuarioEditado) throws Exception {
+        Usuario usuarioExistente = arqUsuario.read(usuarioEditado.getID());
+
+        // Se ele mudou o email, checa se o novo email já tem dono
+        if (!usuarioExistente.getEmail().equals(usuarioEditado.getEmail())) {
+            if (arqUsuario.readEmail(usuarioEditado.getEmail()) != null) {
+                return false; // O novo email escolhido já está em uso por outra pessoa
+            }
+        }
     
+        return arqUsuario.update(usuarioEditado);
+    }
+
+    public boolean excluirUsuario(int idUsuario) throws Exception {
+        // 1. Bloqueia se houver cursos ativos
+        if (arqCurso.verificaUsuarioTemCursos(idUsuario)) {
+            return false;   // A exclusão não é executada e retorna false 
+        }
+
+        // 2. Remove todos os cursos inativos associados  ao usuário
+        ArrayList<Curso> cursosDoUsuario = arqCurso.readAll(idUsuario);
+        for (Curso c : cursosDoUsuario) {
+            arqCurso.delete(c.getID());
+        }
+
+        // 3. Remove o usuário
+        return arqUsuario.delete(idUsuario);
+    }
 }
--- src/entidades/usuarios/VisaoUsuario.java
+++ src/entidades/usuarios/VisaoUsuario.java
@@ -1,5 +1,107 @@
 package entidades.usuarios;
 
+import java.util.Scanner;
+
 public class VisaoUsuario {
-    
+    private ControleUsuario controle;
+    private Scanner console;
+
+    public VisaoUsuario() throws Exception {
+        this.controle = new ControleUsuario();
+        this.console = new Scanner(System.in);
+    }
+
+    public void telaCadastro() {
+        System.out.print("Digite o nome: ");
+        String nome = console.nextLine();
+
+        System.out.print("Digite o email: ");
+        String email = console.nextLine();
+
+        System.out.print("Digite o senha: ");
+        int senha = console.nextLine().hashCode();
+        
+        System.out.print("Digite o pergunta secreta: ");
+        String perguntaSecreta = console.nextLine();
+
+        System.out.print("Digite a resposta secreta: ");
+        int respostaSecreta = console.nextLine().hashCode();
+
+        try {
+            boolean sucesso = controle.cadastrarUsuario(nome,email,senha,perguntaSecreta,respostaSecreta);
+            if (sucesso) {
+                System.out.println("Usuário cadastrado com sucesso!");
+            } else {
+                System.out.println("Erro: O usuário possui cursos ativos e não pode ser excluído.");
+            }
+        } catch (Exception e) {
+            System.out.println("Ocorreu um erro no sistema: " + e.getMessage());
+        }
+    }
+
+    // primeira versão (vai ocorrer alteracoes)
+    public void telaLogin() {
+        System.out.print("Digite o email: ");
+        String email = console.nextLine();
+
+        System.out.print("Digite o senha: ");
+        int senha = console.nextLine().hashCode();
+
+        try {
+            boolean sucesso = (controle.logarUsuario(email,senha) != null) ? true : false;
+            if (sucesso) {
+                System.out.println("Usuário logado");
+            } else {
+                System.out.println("Erro: Email e/ou senha invalidos");
+            }
+        } catch (Exception e) {
+            System.out.println("Ocorreu um erro no sistema: " + e.getMessage());
+        }
+    }
+
+    // primeira versão (vai ocorrer alteracoes)
+    public void telaAtualização() {
+    /*  Usuario usuarioEditado;
+        System.out.print("Digite o nome: ");
+        String nome = console.nextLine();
+
+        System.out.print("Digite o email: ");
+        String email = console.nextLine();
+
+        System.out.print("Digite o senha: ");
+        int senha = console.nextLine().hashCode();
+        
+        System.out.print("Digite o pergunta secreta: ");
+        String perguntaSecreta = console.nextLine();
+
+        System.out.print("Digite a resposta secreta: ");
+        int respostaSecreta = console.nextLine().hashCode();
+
+        try {
+            boolean sucesso = controle.atualizarUsuario(usuarioEditado);
+            if (sucesso) {
+                System.out.println("Usuário cadastrado com sucesso!");
+            } else {
+                System.out.println("Erro: O usuário possui cursos ativos e não pode ser excluído.");
+            }
+        } catch (Exception e) {
+            System.out.println("Ocorreu um erro no sistema: " + e.getMessage());
+        }*/
+    }
+
+    public void telaExclusao() {
+        System.out.print("Digite o ID do usuário que deseja excluir: ");
+        int id = console.nextInt();
+
+        try {
+            boolean sucesso = controle.excluirUsuario(id);
+            if (sucesso) {
+                System.out.println("Usuário e seus cursos inativos foram excluídos com sucesso!");
+            } else {
+                System.out.println("Erro: O usuário possui cursos ativos e não pode ser excluído.");
+            }
+        } catch (Exception e) {
+            System.out.println("Ocorreu um erro no sistema: " + e.getMessage());
+        }
+    }
 }
