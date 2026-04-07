0a0bb6efb84c3866f907ab5a6700b34ed83328bb
Luiz Fernando Santos Langa <luizfernandosantoslanga2006@gmail.com>
2026-04-07 07:13:37 -0300

    User menu

--- src/entidades/usuarios/VisaoUsuario.java
+++ src/entidades/usuarios/VisaoUsuario.java
@@ -11,6 +11,34 @@ public class VisaoUsuario {
         this.console = new Scanner(System.in);
     }
 
+    public void menuUsuario() {
+        System.out.println("╔════════════════════╗");
+        System.out.println("║    Menu Usuario    ║");
+        System.out.println("╚════════════════════╝\n");
+        System.out.println("Opcoes:");
+        System.out.println("1 - Cadastrar");
+        System.out.println("2 - Login");
+        System.out.println("3 - Atualizar Usuario");
+        System.out.println("4 - Excluir Usuario");
+        switch (console.nextInt()) {
+            case 1:
+                telaCadastro();
+                break;
+            case 2:
+                telaLogin();
+                break;
+            case 3:
+                telaAtualização();
+                break;
+            case 4:
+                telaExclusao();
+                break;
+            default:
+                System.out.println("Opção inválida.");
+                break;
+        }
+    }
+
     public void telaCadastro() {
         System.out.print("Digite o nome: ");
         String nome = console.nextLine();
@@ -32,7 +60,7 @@ public class VisaoUsuario {
             if (sucesso) {
                 System.out.println("Usuário cadastrado com sucesso!");
             } else {
-                System.out.println("Erro: O usuário possui cursos ativos e não pode ser excluído.");
+                System.out.println("Erro: Este email já está cadastrado no sistema.");            
             }
         } catch (Exception e) {
             System.out.println("Ocorreu um erro no sistema: " + e.getMessage());
@@ -44,15 +72,19 @@ public class VisaoUsuario {
         System.out.print("Digite o email: ");
         String email = console.nextLine();
 
-        System.out.print("Digite o senha: ");
+        System.out.print("Digite a senha: ");
         int senha = console.nextLine().hashCode();
 
+        Usuario usuarioLogado = new Usuario();
+
         try {
-            boolean sucesso = (controle.logarUsuario(email,senha) != null) ? true : false;
-            if (sucesso) {
-                System.out.println("Usuário logado");
+            usuarioLogado = controle.logarUsuario(email, senha);
+            
+            if (usuarioLogado != null) {
+                System.out.println("Usuário logado com sucesso! Bem-vindo, " + usuarioLogado.getNome());
+                System.out.println("nome: " + usuarioLogado.getNome());
             } else {
-                System.out.println("Erro: Email e/ou senha invalidos");
+                System.out.println("Erro: Email e/ou senha inválidos");
             }
         } catch (Exception e) {
             System.out.println("Ocorreu um erro no sistema: " + e.getMessage());
@@ -61,7 +93,6 @@ public class VisaoUsuario {
 
     // primeira versão (vai ocorrer alteracoes)
     public void telaAtualização() {
-    /*  Usuario usuarioEditado;
         System.out.print("Digite o nome: ");
         String nome = console.nextLine();
 
@@ -77,16 +108,19 @@ public class VisaoUsuario {
         System.out.print("Digite a resposta secreta: ");
         int respostaSecreta = console.nextLine().hashCode();
 
+        // 1. Instanciar o objeto COM o ID do usuário que está logando
+        Usuario usuarioEditado = new Usuario(nome, email, senha, perguntaSecreta, respostaSecreta);
+
         try {
             boolean sucesso = controle.atualizarUsuario(usuarioEditado);
             if (sucesso) {
-                System.out.println("Usuário cadastrado com sucesso!");
+                System.out.println("Usuário atualizado com sucesso!");
             } else {
-                System.out.println("Erro: O usuário possui cursos ativos e não pode ser excluído.");
+                System.out.println("Erro: O novo email escolhido já está em uso por outro usuário.");
             }
         } catch (Exception e) {
             System.out.println("Ocorreu um erro no sistema: " + e.getMessage());
-        }*/
+        }
     }
 
     public void telaExclusao() {
